836b277c809ec318dcee3a85b3ed60197ba02ff3
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-10-31 00:42:34 +0400

    final

--- utility.py
+++ utility.py
@@ -5,17 +5,16 @@ import sys
 from random import randint
 
 def download_images(link: str) -> None:
-    response = requests.get(link)
+    response = requests.get(link).content
     with open(f'image{randint(0,10)}.jpg', "wb") as f:
-        f.write(response.content)
+        f.write(response)
                                              
             
 if __name__ == '__main__':
     t = time.time()
     links = sys.argv[1:]
-    list_links = links.split()
     results = []
-    for link in list_links:
+    for link in links:
         future = concurrent.futures.ProcessPoolExecutor().submit(download_images, link)
         results.append(future)
     print(time.time() - t)    
